4b55a8f7044dab217bc8197bd824a7881d4243c5
Confidence <confi2s7@gmail.com>
2025-06-11 09:40:53 +0100

    jfjf

--- main/about/about.js
+++ main/about/about.js
@@ -1,5 +1,30 @@
  const date = document.getElementById('date'),
 time = document.getElementById('time')
 
-// date.innerText = new Date().toDateString()
-time.innerText = new Date().getMinutes()
+date.innerText = new Date().getFullYear()
+const now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let ampm = 'PM'
+if(hours >= 12){
+    ampm = 'PM'
+}else{
+    ampm = "AM"
+}
+
+if(hours > 12){
+    hours = hours % 12
+}else{
+    hours = hours
+}
+
+if(minutes >= 10){
+    minutes = minutes
+}else{
+    minutes = '0'+ minutes
+}
+
+time.innerText = `${hours}:${minutes} ${ampm}`;
+
+
+
